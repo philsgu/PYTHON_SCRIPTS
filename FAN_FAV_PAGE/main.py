d6b262f56bcd7bcefc50c902734a79c5569f60db
PHIL KIM <philsgu@gmail.com>
2025-05-12 11:14:12 -0700

    update on syntax error line 69

--- FAN_FAV_PAGE/main.py
+++ FAN_FAV_PAGE/main.py
@@ -66,7 +66,7 @@ df_main_poster_raw = load_data_from_url(main_poster_csv_url, "Main Posters")
 
 # --- Display Last Successful Data Fetch Time ---
 if st.session_state.last_successful_data_fetch_time > 0:
-    fetch_time_str = datetime.datetime.fromtimestamp(st.session_state.last_successful_data_fetch_time).strftime(("%Y-%m-%d %H:%M:%S")
+    fetch_time_str = datetime.datetime.fromtimestamp(st.session_state.last_successful_data_fetch_time).strftime(("%Y-%m-%d %H:%M:%S"))
     # PDT is UTC-7
     # pdt_time = utc_time - datetime.timedelta(hours=7)
     # fetch_time_str = pdt_time.strftime("%Y-%m-%d %H:%M:%S")
